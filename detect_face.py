200be0c2e968ab496a76203434bdd93335dc5e33
Natalie Bogda <nbogda@vols.utk.edu>
2019-09-28 21:16:38 -0400

    Done with facial recognition, moving onto chatting

--- detect_face.py
+++ detect_face.py
@@ -5,7 +5,11 @@ from gtts import gTTS
 import pyttsx3
 from playsound import playsound
 import pyaudio
-import speech_recognition as sr
+import ibm_watson
+from ibm_watson import SpeechToTextV1
+from ibm_watson.websocket import RecognizeCallback, AudioSource
+from queue import Queue, Full
+import cv2
 
 def detect_face():
 
@@ -13,8 +17,9 @@ def detect_face():
     engine.setProperty('rate', 150)
     
     images = os.listdir('images')
-    img = face_recognition.load_image_file("new.png")
-    img = face_recognition.face_encodings(img)
+    img_o = face_recognition.load_image_file("new.png")
+    img = face_recognition.face_encodings(img_o)
+    #if there was not a face in the frame
     if not img:
         engine.say("I don't see anyone there.")
         engine.runAndWait()
@@ -24,18 +29,22 @@ def detect_face():
         curr = face_recognition.load_image_file("images/" + image)
         curr = face_recognition.face_encodings(curr)[0]
         result = face_recognition.compare_faces(img, curr)
+        #if face was recognized, greet the person by name
         if result[0]:
             name = re.search(r"(.*).png", image).group(1) 
             engine.say("Hello %s, nice to see you again." % name)
             engine.runAndWait()
             return True
+    #if face was not recognized, ask for name and store face
     engine.say("I don't think I've met you before, what's your name?")
     engine.runAndWait()
-    r = sr.recognize_google()
+    name = input()
+    engine.say("Nice to meet you %s, my name is Jeremy." % name)
+    engine.runAndWait()
+    cv2.imwrite("images/%s.png" % name, img_o) 
     return True
 
-
 if __name__ == "__main__":
-
+   
     if not detect_face():
         print("No face detected")
